12c12d94355833f74c4929678d41f9145174cd9d
neo-mac <miaobingliang@neotrident.com>
2017-08-09 09:46:38 +0800

    首页优化 渲染

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,8 @@ var app = getApp()
 var netWork = require('../../utils/network.js')
 var localData = [
   {
-    imageUrl:'http://omh0qz95c.bkt.clouddn.com/timg%20%281%29.jpeg',
-    name:'名称1',
+    imageUrl: 'http://omh0qz95c.bkt.clouddn.com/timg%20%281%29.jpeg',
+    name: '名称1',
   },
   {
     imageUrl: 'http://omh0qz95c.bkt.clouddn.com/bali1.jpg',
@@ -15,58 +15,50 @@ var localData = [
     imageUrl: 'http://omh0qz95c.bkt.clouddn.com/bali4.jpg',
     name: '名称3',
   },
-  ],
+],
   imageArray = [
   ]
 Page({
   data: {
-   dataArray:[],
-   imageArray:imageArray,
-   /** 
-   * 页面配置 
-   */
-   winWidth: 0,
-   winHeight: 0,
-   // tab切换 
-   currentTab: 0, 
+    dataArray: [],
+    imageArray: imageArray,
+    /** 
+    * 页面配置 
+    */
+    winWidth: 0,
+    winHeight: 0,
+    // tab切换 
+    currentTab: 0,
   },
-  
+
   //事件处理函数
   onLoad: function () {
     var that = this
     wx.showLoading({
       title: '加载中',
     })
-    /** 
-   * 获取系统信息 
-   */
-    netWork.getMainThemeList((data)=>{
-      netWork.getMainScroll((mainImageArray)=>{
-        that.setData({
-          imageArray: mainImageArray
-        })
-      })
-      wx.hideLoading()
+    Promise.all([netWork.getMainScroll(), netWork.getMainThemeList()]).then(([data1,data2])=>{
         netWork.loginWithLeanCloud()
-    wx.getSystemInfo({
+        wx.hideLoading()
+       wx.getSystemInfo({
       success: function (res) {
         wx.hideLoading()
         that.setData({
           winWidth: res.windowWidth,
           winHeight: res.windowHeight,
-          dataArray:data,
+          dataArray:data2,
+          imageArray: data1
         });
       }
 
     }); 
     })
-  
   },
 
-  clickImageidx(e){
+  clickImageidx(e) {
     var idx = e.currentTarget.dataset.type + 1
     wx.navigateTo({
-      url: '../itemList/itemList?type='+idx,
+      url: '../itemList/itemList?type=' + idx,
     })
   },
 })
--- utils/network.js
+++ utils/network.js
@@ -1,175 +1,209 @@
 var networkBaseUrl = '123'
 const AV = require('.././libs/av-weapp-min.js')
 var getTestData = {
-    // 发送短信验证
-    snedShortMessage(phoneNumber,successCallBack,errorCallBack){
-      
-      AV.Cloud.requestSmsCode({
-        mobilePhoneNumber: phoneNumber,
-        name: '超想去旅行',
-        op: '验证',
-        ttl: 10                
-      }).then(function () {
-        successCallBack()
-      }, function (err) {
-        //调用失败
-        
-        errorCallBack(err)
-      });
-},
-//验证短信
-verifyMessageCode(verifyCode,phoneNumber,successCallBack,errorCallBack){
-  AV.Cloud.verifySmsCode(verifyCode,phoneNumber ).then(function () {
-    //验证成功
-    successCallBack()
-  }, function (err) {
-    //验证失败
-    errorCallBack(err)
-  });
-},
-//leancloud login
-loginWithLeanCloud(){
-  var that = this
-  AV.User.loginWithWeapp().then(user => {
-    // this.globalData.user = user.toJSON();
-    that.leanCloudGetUserInfo('123')
-  }).catch(console.error);
-},
-leanCloudGetUserInfo(phoneNumber){
+  // 发送短信验证
+  snedShortMessage(phoneNumber, successCallBack, errorCallBack) {
+
+    AV.Cloud.requestSmsCode({
+      mobilePhoneNumber: phoneNumber,
+      name: '超想去旅行',
+      op: '验证',
+      ttl: 10
+    }).then(function () {
+      successCallBack()
+    }, function (err) {
+      //调用失败
+
+      errorCallBack(err)
+    });
+  },
+  //验证短信
+  verifyMessageCode(verifyCode, phoneNumber, successCallBack, errorCallBack) {
+    AV.Cloud.verifySmsCode(verifyCode, phoneNumber).then(function () {
+      //验证成功
+      successCallBack()
+    }, function (err) {
+      //验证失败
+      errorCallBack(err)
+    });
+  },
+  //leancloud login
+  loginWithLeanCloud() {
+    var that = this
+    AV.User.loginWithWeapp().then(user => {
+      // this.globalData.user = user.toJSON();
+      that.leanCloudGetUserInfo('123')
+    }).catch(console.error);
+  },
+  leanCloudGetUserInfo(phoneNumber) {
     const user = AV.User.current();
     // 调用小程序 API，得到用户信息
     wx.getUserInfo({
-      success: ({userInfo}) => {
+      success: ({ userInfo }) => {
         // 更新当前用户的信息
         userInfo.mobilePhoneNumber = phoneNumber
         user.set(userInfo).save().then(user => {
-          
+
           // 成功，此时可在控制台中看到更新后的用户信息
           // this.globalData.user = user.toJSON();
         }).catch(console.error);
       }
     });
   },
-  getMainScroll(successCallback){
-  var query = new AV.Query('MainScroll')
-  query.descending('createdAt')
-  query.find().then((data)=>{
-    var imageArray = data[0].attributes.imageArray
-    if(imageArray.length){
-      var dataArray = []
-      for(var i=0;i<imageArray.length;i++){
-        dataArray.push({ 'imageUrl': imageArray[i].url})
-      }
-      return successCallback(dataArray)
-    }   
-  })
+  getMainScroll(successCallback) {
+    // var query = new AV.Query('MainScroll')
+    // query.descending('createdAt')
+    // query.find().then((data)=>{
+    //   var imageArray = data[0].attributes.imageArray
+    //   if(imageArray.length){
+    //     var dataArray = []
+    //     for(var i=0;i<imageArray.length;i++){
+    //       dataArray.push({ 'imageUrl': imageArray[i].url})
+    //     }
+    //     return successCallback(dataArray)
+    //   }   
+    // })
+    return new Promise((resolve, reject) => {
+      var query = new AV.Query('MainScroll')
+      query.descending('createdAt')
+      query.find().then((data)=>{
+      var imageArray = data[0].attributes.imageArray
+      if(imageArray.length){
+        var dataArray = []
+        for(var i=0;i<imageArray.length;i++){
+          dataArray.push({ 'imageUrl': imageArray[i].url})
+        }
+        return resolve(dataArray)
+      }   
+    },(error)=>{
+      reject(error)
+    })
+    })
   },
   //获取首页列表
-getMainThemeList(successCallback){
-  var query = new AV.Query('Theme')
-  query.find().then((data)=>{
-    if (data.length) {
-      var dataArray = []
-      for (var i = 0; i < data.length; i++) {
-        var model = data[i]
-        if (model.attributes.imageArray.length) {
-          model.attributes.url = model.attributes.imageArray[0].url
+  getMainThemeList(successCallback) {
+    // var query = new AV.Query('Theme')
+    // query.find().then((data) => {
+    //   if (data.length) {
+    //     var dataArray = []
+    //     for (var i = 0; i < data.length; i++) {
+    //       var model = data[i]
+    //       if (model.attributes.imageArray.length) {
+    //         model.attributes.url = model.attributes.imageArray[0].url
+    //       }
+    //       dataArray.push(data[i].attributes)
+    //     }
+    //   }
+    //   successCallback(dataArray)
+    // }, (error) => {
+
+    // })
+    return new Promise((resolve,reject)=>{
+      var query = new AV.Query('Theme')
+      query.find().then((data) => {
+        if (data.length) {
+          var dataArray = []
+          for (var i = 0; i < data.length; i++) {
+            var model = data[i]
+            if (model.attributes.imageArray.length) {
+              model.attributes.url = model.attributes.imageArray[0].url
+            }
+            dataArray.push(data[i].attributes)
+          }
         }
-        dataArray.push(data[i].attributes)
-      }
-    }
-     successCallback(dataArray)
-  },(error)=>{
-    
-  })
+        return resolve(dataArray)
+      }, (error) => {
+        return reject(error)
+      })
+    })
   },
   //获取列表
-  getItemList(typeSign,successCallback){
+  getItemList(typeSign, successCallback) {
     var query = new AV.Query('Product')
-    query.equalTo('type',typeSign)
-    query.select(['place', 'name','startDate','type','endDate','onleyId','price','describe','imageArray'])
-    query.find().then((data) =>{
-      
-      if(data.length){
+    query.equalTo('type', typeSign)
+    query.select(['place', 'name', 'startDate', 'type', 'endDate', 'onleyId', 'price', 'describe', 'imageArray'])
+    query.find().then((data) => {
+
+      if (data.length) {
         var dataArray = []
-        
+
         for (var i = 0; i < data.length; i++) {
           var model = data[i]
-          
-          if (model.attributes.imageArray.length){
+
+          if (model.attributes.imageArray.length) {
             model.attributes.coverImage = model.attributes.imageArray[0].url
-            
+
           }
           model.attributes.endDate = model.attributes.endDate.toISOString().slice(0, 10)
           model.attributes.startDate = model.attributes.startDate.toISOString().slice(0, 10)
-          
+
           model.attributes.uid = model.id
-        dataArray.push(data[i].attributes)
-        
+          dataArray.push(data[i].attributes)
+
         }
       }
-     
+
       return successCallback(dataArray)
     }, function (error) {
       // 异常处理
     });
   },
   //预定
-  reserveItem(data,successCallBack,errorCallBack){
+  reserveItem(data, successCallBack, errorCallBack) {
     var Product = AV.Object.extend('OrderItem')
     var product = new Product()
-    var targetItem = AV.Object.createWithoutData('Product',data.itemId)
-      product.set('phoneNumber', data.phoneNumber)
-      product.set('name', data.name)
-      product.set('peopleCount', data.peopleCount)
-      product.set('finishDate', data.finishDate)
-      product.set('targetItem',targetItem)
-      product.save().then((todo) => {
-        wx.hideLoading()
-        wx.showToast({
-          title: '提交成功'
-        })
-        setTimeout(function(){
-          wx.navigateBack({
-
-          })
-        },500)
-       
-      }, (error) => {
-          wx.hideLoading()
-          wx.showToast({
-            title: '提交失败'
-          })
-        
+    var targetItem = AV.Object.createWithoutData('Product', data.itemId)
+    product.set('phoneNumber', data.phoneNumber)
+    product.set('name', data.name)
+    product.set('peopleCount', data.peopleCount)
+    product.set('finishDate', data.finishDate)
+    product.set('targetItem', targetItem)
+    product.save().then((todo) => {
+      wx.hideLoading()
+      wx.showToast({
+        title: '提交成功'
       })
-    
+      setTimeout(function () {
+        wx.navigateBack({
+
+        })
+      }, 500)
+
+    }, (error) => {
+      wx.hideLoading()
+      wx.showToast({
+        title: '提交失败'
+      })
+
+    })
+
   },
   //根据id获取信息详情
-  getDetailItemWithId(onlyId,successCallback,errorCallback){
-  var query = new AV.Query('Product')
-  query.equalTo('_id',onlyId)
-  query.find().then((data)=>{
-    if(data.length){
-      var model = data[0]
-      model.attributes.uid = model.id
-      model.attributes.startDate = model.attributes.startDate.toISOString().slice(0, 10)
-      model.attributes.endDate = model.attributes.endDate.toISOString().slice(0, 10)   
-      successCallback(model.attributes)
-    }
-  },(error)=>{
-    wx.showToast({
-      title: '获取信息失败，请重试',
+  getDetailItemWithId(onlyId, successCallback, errorCallback) {
+    var query = new AV.Query('Product')
+    query.equalTo('_id', onlyId)
+    query.find().then((data) => {
+      if (data.length) {
+        var model = data[0]
+        model.attributes.uid = model.id
+        model.attributes.startDate = model.attributes.startDate.toISOString().slice(0, 10)
+        model.attributes.endDate = model.attributes.endDate.toISOString().slice(0, 10)
+        successCallback(model.attributes)
+      }
+    }, (error) => {
+      wx.showToast({
+        title: '获取信息失败，请重试',
+      })
     })
-  })
   },
 }
-function handleDataWithNoPostData(dataUrl,success,error){
+function handleDataWithNoPostData(dataUrl, success, error) {
   wx.request({
     url: dataUrl,
-    success:function(data){
+    success: function (data) {
       return success(data)
     },
-    error:function(data){
+    error: function (data) {
       return error(data)
     },
   })
