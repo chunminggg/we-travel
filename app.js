a5180dde360e9861d6bcb704583b9878e74524f8
neo-mac <miaobingliang@neotrident.com>
2017-08-28 07:56:52 +0800

    done

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 //app.js
 const AV = require('./libs/av-weapp-min.js')
+var mta = require('./libs/mta_analysis.js')
 AV.init({
   appId: 'qDUQr0EmHHn3HOIqb3Re0IHa-gzGzoHsz',
   appKey: 'w2TRHW0KHUkt5mVHtgp9wa2s',
 })
+
 App({
-  onLaunch: function() {
+  onLaunch: function(options) {
     //调用API从本地缓存中获取数据
+
+    mta.App.init({
+      "appID": "500512368",
+      "eventID": "500512369",
+      "statShareApp": true,
+      "lauchOpts": options,
+    });
     // var logs = wx.getStorageSync('logs') || []
     // logs.unshift(Date.now())
     // wx.setStorageSync('logs', logs)
