bf34b13a0d07fa184c8503f1164ca23441ca4cd4
neo-mac <miaobingliang@neotrident.com>
2017-09-27 16:24:24 +0800

    价格视图

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -18,6 +18,9 @@ Page({
     offsetTop: 0,
     imageArray: [],
     countNumber: 0,
+    //price view
+    showModalStatus: false,
+    tagArray:[],
     myList: [
       {
         id: 'form',
@@ -65,7 +68,7 @@ Page({
       }
     ]
   },
-
+  
   getDetailData(onlyId) {
     wx.showLoading({
       title: '数据加载中',
@@ -79,7 +82,9 @@ Page({
         localImageArray = data.imageArray
       }
       wx.hideLoading()
+
       that.setData({
+        tagArray:data.tagArray,
         detailData: data,
         icon60: data.imageArray[0].url,
         imageArray: localImageArray,
@@ -173,6 +178,61 @@ Page({
     wx.navigateTo({
       url: '../reserve/reserve?itemId=' + onlyId,
     })
+  },
+  closePriceView(e){
+
+  },
+  showPriceView(e){
+    let currentStatu = e.currentTarget.dataset.statu;
+    this.util(currentStatu)
+  },
+  util: function (currentStatu) {
+    /* 动画部分 */
+    // 第1步：创建动画实例   
+    var animation = wx.createAnimation({
+      duration: 200,  //动画时长  
+      timingFunction: "linear", //线性  
+      delay: 0  //0则不延迟  
+    });
+    // 第2步：这个动画实例赋给当前的动画实例  
+    this.animation = animation;
+    // 第3步：执行第一组动画  
+    animation.opacity(0).rotateX(-100).step();
+    // 第4步：导出动画对象赋给数据对象储存  
+    this.setData({
+      animationData: animation.export()
+    })
+    // 第5步：设置定时器到指定时候后，执行第二组动画  
+    setTimeout(function () {
+      // 执行第二组动画  
+      animation.opacity(1).rotateX(0).step();
+      // 给数据对象储存的第一组动画，更替为执行完第二组动画的动画对象  
+      this.setData({
+        animationData: animation
+      })
+
+
+
+
+
+      //关闭  
+      if (currentStatu == "close") {
+        this.setData(
+          {
+            showModalStatus: false
+          }
+        );
+      }
+    }.bind(this), 200)
+
+    // 显示  
+    if (currentStatu == "open") {
+      this.setData(
+        {
+          showModalStatus: true
+        }
+      );
+    }
   },
   /**
    * 生命周期函数--监听页面初次渲染完成
